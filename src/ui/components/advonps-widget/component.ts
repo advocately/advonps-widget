c35af10d6fc06e8661a7d3ebea0be94f566a5725
Lachlan Priest <lachlanpriest@gmail.com>
2017-10-02 13:53:49 +0800

    Hide by default and show after 3 seconds

--- src/ui/components/advonps-widget/component.ts
+++ src/ui/components/advonps-widget/component.ts
@@ -3,5 +3,16 @@ import Component from '@glimmer/component';
 export default class AdvonpsWidget extends Component {
   hideSurvey() {
     this.element.className = "advonps-survey nps-slide--leave";
+  },
+
+  didInsertElement() {
+    if (window.advocately) {
+      setTimeout(() => {
+        this.element.className = "advonps-survey";
+      }, 3000);
+    } else {
+      this.element.className = "advonps-survey";
+    }
   }
 };
+
